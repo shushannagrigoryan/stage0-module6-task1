c25506b20e593a3b2899b29e3afd653db524a0f7
Shushanna <shushannagrigoryan7@gmail.com>
2022-09-07 17:36:13 +0400

    1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,23 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(){}
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    //Add a methods getDescription(), witch would use class fields and return a string with such pattern
+    // "This animal is mostly (color). It has (numberOfPaws) paws and ('a'/'no' -> depends on value of hasFur) fur."
+    public String getDescription(){
+        String fur = hasFur? "a":"no";
+        String pawChanged = numberOfPaws == 1?"paw":"paws";
+        return "This animal is mostly " + color + "." + " It has " + numberOfPaws + " " +pawChanged + " and " + fur + " fur.";
+    }
 
 }
